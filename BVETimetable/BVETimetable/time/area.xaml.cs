ab7587072afa4aa959e4bf04021b298a25b6bfed
福田 拳士朗 <jr-e-e653-kenshiro@outlook.com>
2018-03-19 22:27:52 +0900

    昨日の成果部分　1を完成

--- BVETimetable/BVETimetable/time/area.xaml.cs
+++ BVETimetable/BVETimetable/time/area.xaml.cs
@@ -14,12 +14,12 @@ namespace BVETimetable.time
     public partial class area : ContentPage
     {
         // 任意のListViewを生成する
-        ObservableCollection<ItemClass> _page1 = new ObservableCollection<ItemClass>();
+        ObservableCollection<string> _page1 = new ObservableCollection<string>();
 
         //任意の文字列の設定（ここに追加する）
         public string[] _loacls =
         {
-            "北海道","東北",　"東京近郊",//"東京電車特定"
+            "北海道","東北",　"東京近郊"//"東京電車特定"
         };
         public string _select;
 
@@ -31,46 +31,41 @@ namespace BVETimetable.time
 
             for (int i = 0; i < _loacls.Length; i++)
             {
-                _page1.Add(new ItemClass { Word = _loacls[i] });
+                _page1.Add( _loacls[i] );
             }
             arealist.ItemsSource = _page1;
 
             //セル選択→上から行って、北海道フォルダのページへ画面推移
 
-            arealist.ItemTapped += async (sender, args) => {
-                var word = (arealist.SelectedItem as ItemClass).Word;
-                Page targetPage;
+            arealist.ItemTapped += async (sender, e) => {
+
+                _select = (arealist.SelectedItem as string);
+                switch (_select)
 
 
-                switch (word)
+              
                 {
                     case "北海道":
-                        targetPage = new hokkaido.kouro();
+                        await Navigation.PushModalAsync(new hokkaido.kouro());
                         break;
 
                     case "東北":
-                        targetPage = new tohoku.kouro();
+                        await Navigation.PushModalAsync(new tohoku.kouro());
                         break;
 
-                        
+                    default:
+                        break;
 
                         //同様の処理をさせる
 
                 }
 
                 
-                //await Navigation.PushModalAsync(targetPage);
-                await DisplayAlert("Item Tapped", "An item was tapped.", "OK");//だと全部アラート出る
 
 
             };
 
         }
-    }
-
-    public class ItemClass
-    {
-        public string Word { get; set; }
     }
 
 
