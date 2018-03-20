9cfdba9aed45e211ab00e6c45524b86a882037af
KENSHIRO\KENSHIRO <jr-e-e653-kenshiro@outlook.com>
2018-03-20 10:42:53 +0900

    ページ追加

--- BVETimetable/BVETimetable/time/hokkaido/kouro.xaml.cs
+++ BVETimetable/BVETimetable/time/hokkaido/kouro.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,58 @@ using Xamarin.Forms.Xaml;
 
 namespace BVETimetable.time.hokkaido
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class kouro : ContentPage
-	{
-		public kouro ()
-		{
-			InitializeComponent ();
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class kouro : ContentPage
+    {
+        // 任意のListViewを生成する
+        ObservableCollection<string> _Hkouro = new ObservableCollection<string>();
 
-		}
-	}
+        //任意の文字列の設定（ここに追加する）
+        public string[] _loacls =
+        {
+            "N1","東北","N3"//"東京電車特定"
+        };
+        public string _select;
+        public kouro()
+        {
+            InitializeComponent();
+            // iOSだけ、上部に余白をとる
+            Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
+
+            for (int i = 0; i < _loacls.Length; i++)
+            {
+                _Hkouro.Add(_loacls[i]);
+            }
+            kourolist.ItemsSource = _Hkouro;
+
+            //セル選択→上から行って、北海道フォルダのページへ画面推移
+
+            kourolist.ItemTapped += async (sender, e) =>
+            {
+
+                _select = (kourolist.SelectedItem as string);
+                switch (_select)
+
+
+
+                {
+                    case "N1":
+                        await Navigation.PushModalAsync(new hokkaido.Page1());
+                        break;
+
+                    case "東北":
+                        await Navigation.PushModalAsync(new tohoku.kouro());
+                        break;
+
+                    default:
+                        break;
+
+                            //同様の処理をさせる
+
+                    }
+
+            };
+
+        }
+    }
 }
