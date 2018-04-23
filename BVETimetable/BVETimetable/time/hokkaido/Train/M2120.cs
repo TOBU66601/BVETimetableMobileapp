eccabc5165c4db8417c627a50aa0fbb2070f46c0
KENSHIRO\KENSHIRO <jr-e-e653-kenshiro@outlook.com>
2018-04-23 17:22:47 +0900

    夜の仕込みOK

--- BVETimetable/BVETimetable/time/hokkaido/Train/M2120.cs
+++ BVETimetable/BVETimetable/time/hokkaido/Train/M2120.cs
@@ -11,10 +11,12 @@ namespace BVETimetable.time.hokkaido.Train
 	{
 		public M2120 ()
 		{
+           //TitleをC#で記述
+
 
             var webView = new WebView
             {
-                Source = "https://goo.gl/forms/xdxTn12ETjjBFHyF2"
+                Source = "https://goo.gl/forms/xdxTn12ETjjBFHyF2" //ここをファイル名変更。（当分はてすとURL）
             };
             Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
             Content = webView;
