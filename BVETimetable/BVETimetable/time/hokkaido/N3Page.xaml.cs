a8162f2f50e921015bfd6842bb65e98c201a636d
KENSHIRO\KENSHIRO <jr-e-e653-kenshiro@outlook.com>
2018-05-16 12:46:28 +0900

    N3行路追加

---
+++ BVETimetable/BVETimetable/time/hokkaido/N3Page.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BVETimetable.time.hokkaido
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class N3Page : ContentPage
+	{
+		public N3Page ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
